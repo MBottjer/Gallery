83d4e7c78ae74dd7d700247588051b2ac64edcae
Michael Bottjer <michaelbottjer@hotmail.com>
2014-02-06 13:32:33 +0000

    customised foundation orbit slider to transition at regular intervals

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,9 @@
-// $( document ).ready(function() {
-  
-// });
+$(document).foundation({
+  orbit: {
+      animation:slide
+                  animation_speed:1000
+                  pause_on_hover:true
+                  animation_speed:500
+                  navigation_arrows:true
+  }
+});
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @first_product = Product.first
+    @second_product = Product.all[1]
   end
 end
