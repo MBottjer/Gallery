4113586b371693c45aa5c35d76cc6e7f85d8525f
Michael Bottjer <michaelbottjer@hotmail.com>
2014-02-06 13:36:32 +0000

    refactored gallery uploads

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,9 +1,9 @@
 $(document).foundation({
   orbit: {
       animation:slide
-                  animation_speed:1000
-                  pause_on_hover:true
-                  animation_speed:500
-                  navigation_arrows:true
+      animation_speed:1000
+      pause_on_hover:true
+      animation_speed:500
+      navigation_arrows:true
   }
 });
