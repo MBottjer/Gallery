0c1f012ac4d24b5d53dd3827646deef0bbb3cdbd
Michael Bottjer <michaelbottjer@hotmail.com>
2014-03-21 20:49:37 +0000

    removed js causing error (handled gallery options)

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,11 +1,1 @@
-$(document).foundation({
-  orbit: {
-      animation:slide
-      animation_speed:1000
-      pause_on_hover:true
-      animation_speed:500
-      navigation_arrows:true
-      slide_number:false,
-  }
-});
 
