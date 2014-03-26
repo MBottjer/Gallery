da433697c2ddbac924cce0e8e7a88f7842c38c77
Michael Bottjer <michaelbottjer@hotmail.com>
2014-03-26 13:23:48 +0000

    further playing around with paperclip settings

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,1 @@
-Paperclip::Attachment.default_options[:url] = 'countryrosefurniture.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:s3_host_name] = 'countryrosefurniture.s3-website-us-west-2.amazonaws.com'
