4753b7269d40bde9cef02dc81e397c3cba1f67bc
Michael Bottjer <michaelbottjer@hotmail.com>
2014-03-26 13:51:33 +0000

    changed product model specifications

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,13 @@ class Product < ActiveRecord::Base
   validates :description, presence: true
   
 
-  has_attached_file :photo, :styles => { :small => "50x50>", :large => "980x620>"}
+
+  has_attached_file :photo, :styles => { :small => "50x50>", :large => "980x620>"},
+                    :storage => :s3,
+                    :server => 'countryrosefurniture.amazonaws.com',
+                    :bucket => countryrosefurniture,
+                    :path => "photos/:id/:styles",
+                    :s3_credentials => {:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                                        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
   validates_attachment_presence :photo
 end
